7579c0134d539ec553fdfc3c7f22e69e2fd9efde
B1sh0p <bbluebaugh@gmail.com>
2021-10-14 19:04:53 -0400

    @author B1sh0p Initialize repository.

---
+++ rank.py
@@ -0,0 +1,16 @@
+'''
+@author B1sh0p
+@version 1.0.0
+
+This project is designed to rank memes based on popularity and apply a fictional
+value to them. The main goal is to have the ability to trade them like a stock
+market where the price is determined based on the popularity, initially I am
+thinking the way to do this is to cross reference reddit with imgur and determine
+a formula for "price" based on upvote/like to downvote/dislike ratio factoring
+in the share ratio. The next part, I plan to start everyone with a base amount
+of money so that they can start buying and selling immediately, additionally,
+do not want real world money playing a factor so I plan to develop some kind of
+clicker game to go along with it where depending on your clicking you can gain
+in game money to buy more with. Will try to think of a way around macros that
+simulate many clicks per second.
+'''
